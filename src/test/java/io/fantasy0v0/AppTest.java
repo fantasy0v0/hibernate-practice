e6e2b40ecd1d9e091f7b319e1790b3dd1cb66f37
fan <xiaofanms@outlook.com>
2022-08-12 10:31:01 +0800

    初始提交。

---
+++ src/test/java/io/fantasy0v0/AppTest.java
@@ -0,0 +1,15 @@
+package io.fantasy0v0;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+  @Test
+  void test() {
+    System.out.println("测试");
+  }
+}
