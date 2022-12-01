53f5522a3d3a7e3af174a43798e297550ca98b6f
fan <xiaofanms@outlook.com>
2022-12-01 16:58:48 +0800

    完善README.md。

--- src/main/kotlin/io/fantasy0v0/controller/TestController.kt
+++ src/main/kotlin/io/fantasy0v0/controller/TestController.kt
@@ -1,24 +1,51 @@
 package io.fantasy0v0.controller
 
 import io.fantasy0v0.helper.ClazzHelper
+import io.fantasy0v0.po.clazz.ClazzRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import javax.transaction.Transactional
 
 @RestController
 open class TestController(
-  @Autowired private val clazzHelper: ClazzHelper
+  @Autowired private val clazzHelper: ClazzHelper,
+  @Autowired private val clazzRepository: ClazzRepository
 ) {
 
+  private val log: Logger = LoggerFactory.getLogger(TestController::class.java)
+
   /**
-   * 没有事务的情况下, 不会提交对实体类的修改
+   * 没有事务的情况下, 不会自动提交对实体类的修改
    */
   @GetMapping("/test")
   open fun test(): String {
     val clazz = clazzHelper.create()
+    log.info(String.format("%s %s", clazz.id, clazz.name))
     clazz.name = "modify"
     return String.format("%s %s", clazz.id, clazz.name)
+  }
+
+  /**
+   * 参考上一个方法, 在没有事务的情况下, 需要手动调用save方法才会提交对entity的修改
+   */
+  @GetMapping("/test-with-save")
+  open fun testWithSave(): String {
+    val clazz = clazzHelper.create()
+    log.info(String.format("%s %s", clazz.id, clazz.name))
+    clazz.name = "modify"
+    clazzRepository.save(clazz)
+    return String.format("%s %s", clazz.id, clazz.name)
+  }
+
+  @GetMapping("/findById")
+  open fun findById(@RequestParam id: Long): String {
+    val clazz = clazzRepository.findByIdOrNull(id) ?: return "Nothing"
+    return "id:${clazz.id} name:${clazz.name}"
   }
 
   /**
@@ -31,5 +58,4 @@ open class TestController(
     clazz.name = "modify"
     return String.format("%s %s", clazz.id, clazz.name)
   }
-
 }
